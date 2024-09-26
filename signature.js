80d41218e1a324fbbe4e31b6baf52698bcd76fbe
N3rd <landonmarting@icloud.com>
2024-09-26 11:48:42 -0500

    Update signature.js

--- signature.js
+++ signature.js
@@ -6,14 +6,11 @@ const button = document.getElementById('myButton');
 const paragraph = document.getElementById('myParagraph');
 
 // Define the array of possible lines
+//past the folowing 
 const lines = [
   'Hello, World!',
-  'chiken in five worlds of gods and men controll the emotions of the men and the life and will of reality.',
-  'the edges hurt when i think it it it it hurts to blead in the computer it itit willl alsways be help me i iiiiiiii the eges cut when i think.',
-  'I swear if another lesbian couple get together in my cabin I will blow some brains out',
-  'yesterday upon the stairs i meght a man who was not there he wasent there again today oh how i wish hed go away.',
-  'I got a weren stuck on a robot.',
-  'Geeze you tattoed my name  on your lowr back more people have seen that then the phone book.', 
+  'add here',
+  
 ];
 
 // Add an event listener to the button
